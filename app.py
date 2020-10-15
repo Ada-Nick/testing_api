75069d01147de8bdbbc1687782faee50c1eabfee
Samara <samaravazquezperez@ada-mode.com>
2020-10-15 09:24:46 +0100

    updated python version

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!\n'
 
-@app.route('/hello/<username>') # dynamic route
-def hello_user(username):
-    return 'Why Hello %s!\n' % username
+@app.route('/hello/<Samara>') # dynamic route
+def hello_user(Samara):
+    return 'Why Hello %s!\n' % Samara
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug= True)
